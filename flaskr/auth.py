4237a67bc47008797156f80abd2fbce295189a8f
quynt <quy.nt@teko.vn>
2022-04-24 01:14:49 +0700

    add login and register api

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,27 +1,22 @@
 from flask import Blueprint, redirect, url_for, request, session, flash, render_template
 from werkzeug.security import check_password_hash, generate_password_hash
-from .models import db, User
+from flaskr.models import db, User
 from sqlalchemy.exc import IntegrityError
+from typing import Optional, Union
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-def _validate_username_password(username, password, error):
-    if not username:
-        error = 'Username is required.'
-    elif not password:
-        error = 'Password is required.'
-    return error
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        error = None
+        username = request.json['username']
+        password = request.json['password']
+        confirm_password = request.json['confirm_password']
 
         error = _validate_username_password(
-            username,
-            password
+            username=username,
+            password=password,
+            confirm_password=confirm_password,
         )
 
         if error is None:
@@ -34,25 +29,91 @@ def register():
                 db.session.commit()
             except IntegrityError:
                 error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("hello"))
-        flash(error)
-    return render_template('auth/register.html', error=error)
+        
+        if error:
+            return {
+                "message": error
+            }, 400
 
-# @bp.route('/login', methods=('GET', 'POST'))
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         error = None
+        return {
+            "message": "OK"
+        }, 201
+            
+    else:
+        return {
+            "message": "Method is not supported"
+        }, 405
 
-#         error = _validate_username_password(
-#             username,
-#             password
-#         )
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-#         if error is None:
-#             user = User.query.filter(username=username).first()
-#             if user:
+        error = _validate_username_password(
+            username=username,
+            password=password,
+        )
 
+        if error is None:
+            user = User.query.filter_by(username=username).first()
+            if user is None:
+                error = "Incorrect username"
+            elif not check_password_hash(user.password, password):
+                error = "Incorrect password"
 
+        if error:
+            return {
+                "message": error
+            }, 400
+
+        session.clear()
+        session['user_id'] = user.id
+        return {
+            "message": "OK"
+        }, 200
+
+    else:
+        return {
+            "message": "Method is not supported"
+        }, 405
+
+def _validate_username_password(
+    username: str, 
+    password: str, 
+    confirm_password: Optional[str] = None,
+    ) -> Union[str, None]:
+    error = _check_required(username, password)
+
+    if error:
+        return error
+
+    if confirm_password:
+        error = _check_confirm_password(password, confirm_password)
+    return error
+
+def _check_required(
+    username: str, 
+    password: str, 
+    ) -> Union[str, None]:
+    '''
+        Check if username and password attributes exist
+    '''
+    error = None
+
+    if not username:
+        error = 'Username is required.'
+
+    elif not password:
+        error = 'Password is required.'
+    return error
+
+def _check_confirm_password(
+    password: str, 
+    confirm_password: str,
+    ) -> Union[str, None]:
+    error = None
+
+    if password != confirm_password:
+        error = "The password and confirm password are not the same."
+    return error
