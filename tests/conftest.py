bc1414a08dd6aeedcd9c1a57559a7e6fed1a3c06
quynt <quy.nt@teko.vn>
2022-04-26 13:32:00 +0700

    setup test

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import os
+import tempfile
+import pytest
+from flaskr  import create_app
+from flaskr.models import db
+
+@pytest.fixture
+def app():
+    db_fd, db_path = tempfile.mkstemp()
+    app = create_app()
+    app.config.update({
+        'TESTING': True,
+        'SQLALCHEMY_DATABASE_URI': f"sqlite:///{db_path}",
+    })
+
+    with app.app_context():
+        db.create_all()
+    yield app
+
+    os.close(db_fd)
+    os.unlink(db_path)
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
